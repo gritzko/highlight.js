80f1fcdfc2832b4b51b7b37e56711dc1d1902e8c
Eemeli Aro <eemeli@gmail.com>
2015-01-28 15:03:51 +0200

    Adding tags, split inline into separate subLanguage

--- src/languages/markdown.js
+++ src/languages/markdown.js
@@ -1,6 +1,6 @@
 /*
 Language: Markdown
-Requires: xml.js
+Requires: markdown-inline.js, xml.js
 Author: John Crepezzi <john.crepezzi@gmail.com>
 Website: http://seejohncode.com/
 Category: common
@@ -10,100 +10,77 @@ function(hljs) {
   return {
     aliases: ['md', 'mkdown', 'mkd'],
     contains: [
-      // highlight headers
+      // headers
+      {
+        className: 'header-n',
+        begin: /^#{3,6}\s*/, end: /$/,
+        contains: [ { className: 'header', endsWithParent: true } ]
+      },
+      {
+        className: 'header-2',
+        begin: /^##\s*/, end: /$/,
+        contains: [ { className: 'header', endsWithParent: true } ]
+      },
+      {
+        className: 'header-1',
+        begin: /^#\s*/, end: /$/,
+        contains: [ { className: 'header', endsWithParent: true } ]
+      },
       {
         className: 'header',
-        variants: [
-          { begin: '^#{1,6}', end: '$' },
-          { begin: '^.+?\\n[=-]{2,}$' }
-        ]
+        begin: '^.+?\\n[=-]{2,}$'
       },
-      // inline html
+
+      // lists
       {
-        begin: '<', end: '>',
-        subLanguage: 'xml',
-        relevance: 0
-      },
-      // lists (indicators only)
-      {
-        className: 'bullet',
-        begin: '^([*+-]|(\\d+\\.))\\s+'
-      },
-      // strong segments
-      {
-        className: 'strong',
-        begin: '[*_]{2}.+?[*_]{2}'
-      },
-      // emphasis segments
-      {
-        className: 'emphasis',
-        variants: [
-          { begin: '\\*.+?\\*' },
-          { begin: '_.+?_'
-          , relevance: 0
+        className: 'list-item',
+        begin: /^[ \t]*([*+-]|(\d+\.))[ \t]+/, end: '$',
+        returnBegin: true,
+        contains: [
+          {
+            className: 'bullet',
+            begin: /^[ \t]*([*+-]|(\d+\.))[ \t]+/
+          },
+          {
+            subLanguage: 'markdown-inline',
+            endsWithParent: true,
+            relevance: 0
           }
         ]
       },
+
       // blockquotes
       {
         className: 'blockquote',
-        begin: '^>\\s+', end: '$'
-      },
-      // code snippets
-      {
-        className: 'code',
-        variants: [
-          { begin: '`.+?`' },
-          { begin: '^( {4}|\t)', end: '$'
-          , relevance: 0
+        begin: '^>[ \\t]+', end: '$',
+        contains: [
+          {
+            subLanguage: 'markdown-inline',
+            endsWithParent: true,
+            relevance: 0
           }
         ]
       },
+
+      // code snippets
+      {
+        className: 'code',
+        begin: '^( {4}|\t)', end: '$',
+        relevance: 0
+      },
+
       // horizontal rules
       {
         className: 'horizontal_rule',
         begin: '^[-\\*]{3,}', end: '$'
       },
-      // using links - title and link
+
+      // everything else
       {
-        begin: '\\[.+?\\][\\(\\[].*?[\\)\\]]',
-        returnBegin: true,
-        contains: [
-          {
-            className: 'link_label',
-            begin: '\\[', end: '\\]',
-            excludeBegin: true,
-            returnEnd: true,
-            relevance: 0
-          },
-          {
-            className: 'link_url',
-            begin: '\\]\\(', end: '\\)',
-            excludeBegin: true, excludeEnd: true
-          },
-          {
-            className: 'link_reference',
-            begin: '\\]\\[', end: '\\]',
-            excludeBegin: true, excludeEnd: true
-          }
-        ],
-        relevance: 10
+        begin: '^.', end: '$',
+        subLanguage: 'markdown-inline',
+        relevance: 0
       },
-      {
-        begin: '^\\[\.+\\]:',
-        returnBegin: true,
-        contains: [
-          {
-            className: 'link_reference',
-            begin: '\\[', end: '\\]:',
-            excludeBegin: true, excludeEnd: true,
-            starts: {
-              className: 'link_url',
-              end: '$'
-            }
-          }
-        ]
-      }
     ]
   };
 }
